3c8a21395830b39d24fe49c5dfb66c7c6b6a32fb
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2026-04-03 14:03:03 +1000

    Check Auth State

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavMenu from "./NavMenu";
+import AuthProvider from "./AuthProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,13 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-    >
-      <body className="min-h-full flex flex-col">
-        <NavMenu />
-        {children}
-        </body>
-    </html>
+    <AuthProvider>
+      <html
+        lang="en"
+      >
+        <body className="min-h-full flex flex-col">
+          <NavMenu />
+          {children}
+          </body>
+      </html>
+    </AuthProvider>
   );
 }
